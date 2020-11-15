248b077abdb058bb11e608cf6d1cfbcaec0ff1e1
Westscroll <74497425+Westscroll@users.noreply.github.com>
2020-11-15 18:06:45 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '48)naguj$x8450tv$78&7fm=^h#@vkq4x&(d84itlvz0aj*%o*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
